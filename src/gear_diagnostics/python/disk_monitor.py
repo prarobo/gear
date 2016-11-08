4e9a0218262182a957ae94909c3e3d9ffe2bd329
Prasanna Kannappan <kan.prasanna@gmail.com>
2016-11-08 14:46:28 -0500

    Tested disk diagnostics with restrecturing of diagnostics launch files

--- src/gear_diagnostics/python/disk_monitor.py
+++ src/gear_diagnostics/python/disk_monitor.py
@@ -8,6 +8,8 @@ import rospy
 import os
 from diagnostic_updater import DiagnosedPublisher, Updater, FrequencyStatusParam, TimeStampStatusParam
 from diagnostic_updater._publisher import HeaderlessTopicDiagnostic
+from std_msgs.msg._Float32 import Float32
+from diagnostic_msgs.msg import DiagnosticStatus
 
 DEFAULT_MOUNT_POINT = "/mnt/md0"
 DEFAULT_UPDATE_INTERVAL = 60    #seconds
@@ -28,9 +30,12 @@ class MessageMonitor(object):
         # Add disk check diagnostic task
         self.disk_updater.add("Memory check", self._diskCheckTask)
 
+        # Publisher for disk space
+        self.disk_pub = rospy.Publisher("disk_space", Float32, queue_size=1)
+
         # Timer to periodically update diagnostics
         rospy.Timer(rospy.Duration(self.update_interval), self._updateDiagnostics)
-
+        
         rospy.spin()
         return
         
@@ -39,15 +44,15 @@ class MessageMonitor(object):
         '''
         Load ros parameters from parameter server
         '''
-        self.mount_point = rospy.get_param("mount_point", DEFAULT_MOUNT_POINT)
-        self.update_interval = rospy.get_param("update_interval", DEFAULT_UPDATE_INTERVAL)
-        self.low_free_space = rospy.get_param("low_free_space", DEFAULT_LOW_FREE_SPACE)
-        self.critical_free_space = rospy.get_param("critical_free_space", DEFAULT_CRITICAL_FREE_SPACE)
+        self.mount_point = rospy.get_param("~mount_point", DEFAULT_MOUNT_POINT)
+        self.update_interval = rospy.get_param("~update_interval", DEFAULT_UPDATE_INTERVAL)
+        self.low_free_space = rospy.get_param("~low_free_space", DEFAULT_LOW_FREE_SPACE)
+        self.critical_free_space = rospy.get_param("~critical_free_space", DEFAULT_CRITICAL_FREE_SPACE)
         
         rospy.loginfo("[DiskMonitor] mount point : "+self.mount_point)
-        rospy.loginfo("[DiskMonitor] update interval : "+self.update_interval)
-        rospy.loginfo("[DiskMonitor] low free space : "+self.low_free_space)
-        rospy.loginfo("[DiskMonitor] critical free space : "+self.critical_free_space)
+        rospy.loginfo("[DiskMonitor] update interval : "+str(self.update_interval))
+        rospy.loginfo("[DiskMonitor] low free space : "+str(self.low_free_space))
+        rospy.loginfo("[DiskMonitor] critical free space : "+str(self.critical_free_space))
         return
     
     def _getAvailableDiskSpace(self):
@@ -62,17 +67,20 @@ class MessageMonitor(object):
         Check if disk has sufficient space
         '''
         if self._getAvailableDiskSpace() < self.critical_free_space:
-            stat.summary(diagnostic_msgs.msg.DiagnosticStatus.ERROR, "Disk space CRITICAL")
+            stat.summary(DiagnosticStatus.ERROR, "Disk space CRITICAL")
         elif self._getAvailableDiskSpace() < self.low_free_space:
-            stat.summary(diagnostic_msgs.msg.DiagnosticStatus.WARN, "Disk space LOW")
+            stat.summary(DiagnosticStatus.WARN, "Disk space LOW")
         else:
-            stat.summary(diagnostic_msgs.msg.DiagnosticStatus.OK, "Disk space OK")
+            stat.summary(DiagnosticStatus.OK, "Disk space OK")
         return stat
     
     def _updateDiagnostics(self, event):
         '''
         Update diagnostics function
         '''
+        # Send disk space message
+        disk_space = self._getAvailableDiskSpace()
+        self.disk_pub.publish(disk_space)
         
         # Send diagnostics out
         self.disk_updater.update()
