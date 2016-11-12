cb824a11fc1d4e3c85b837b3fd38e2332e255da6
prarobo <kan.prasanna@gmail.com>
2016-11-12 04:41:59 -0500

    Perspectives added

--- gear.sh
+++ gear.sh
@@ -5,6 +5,9 @@ sleep 1
 
 roslaunch gear_launch gear.launch &
 
-rqt
+sleep 8
+rqt -f -l --perspective-file `rosstack find gear_gui`/config/controller.perspective &
+rqt -f -l --perspective-file `rosstack find gear_gui`/config/left.perspective &
+rqt -f -l --perspective-file `rosstack find gear_gui`/config/right.perspective
 
 fg
