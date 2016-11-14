14a84bac38bcdc9f733ff4dc643c1906b5184deb
prarobo <kan.prasanna@gmail.com>
2016-11-14 00:33:23 -0500

    disabling point clouds

--- gear.sh
+++ gear.sh
@@ -3,9 +3,12 @@
 roscore &
 sleep 1
 
-roslaunch gear_launch gear.launch &
+roslaunch gear_launch gear.launch do_pointcloud:=false enable_loggers:=false &
+sleep 2
 
+roslaunch gear_launch logger_gear.launch start_manager:=false nodelet_manager:=gear &
 sleep 8
+
 rqt -f -l --perspective-file `rosstack find gear_gui`/config/controller.perspective &
 rqt -f -l --perspective-file `rosstack find gear_gui`/config/left.perspective &
 rqt -f -l --perspective-file `rosstack find gear_gui`/config/right.perspective
