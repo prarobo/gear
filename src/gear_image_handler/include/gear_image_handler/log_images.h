ba10de24d4e8f35ca28054223dbc4dd214adb507
prarobo <kan.prasanna@gmail.com>
2016-11-22 23:55:16 -0500

    Refactoring of loggers and support for new filestructure

--- src/gear_image_handler/include/gear_image_handler/log_images.h
+++ src/gear_image_handler/include/gear_image_handler/log_images.h
@@ -1,22 +1,17 @@
 #ifndef LOG_IMAGES_H_
 #define LOG_IMAGES_H_
 
+#include <gear_image_handler/logger.h>
+
 // ROS Dependencies
-#include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 #include <sensor_msgs/Image.h>
-#include <std_srvs/SetBool.h>
-#include <std_srvs/Trigger.h>
-
-// Boost Dependencies
-#include <boost/filesystem.hpp>
-#include <boost/thread/mutex.hpp>
 
 namespace gear_image_handler {
 
-class ImageLogger : public nodelet::Nodelet {
+class ImageLogger : public nodelet::Nodelet, public Logger {
 
 public:
   ImageLogger();
@@ -29,51 +24,15 @@ public:
   void onInit();
 
   /**
-   * Initialized the directories where images are to be stored
-   */
-  void initializeSessionDirectories();
-
-  /**
      Image callback function
    **/
   void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 
-  /**
-   * Image logger service callback
-   */
-  bool toggleLogger(std_srvs::SetBool::Request  &req,
-                    std_srvs::SetBool::Response &res);
-
-  /**
-   * Session information service callback
-   */
-  bool setSessionInfo(std_srvs::Trigger::Request  &req,
-                      std_srvs::Trigger::Response &res);
-
 private:
   boost::shared_ptr<image_transport::ImageTransport> it_;
   image_transport::Subscriber image_sub_;
-  ros::Publisher image_count_pub_;
-  ros::ServiceServer toggle_logger_;
-  ros::ServiceServer session_info_;
 
-  std::string session_id_;
-  std::string activity_id_;
-  int trial_id_;
-
-  std::string sensor_id_;
-  std::string data_dir_;
-  std::string image_extn_;
-  std::string image_type_;
-  std::string base_name_;
   std::string encoding_;
-
-  bool enable_;
-  boost::recursive_mutex enable_lock_, session_param_lock_;
-
-  int image_count_;
-
-  boost::filesystem::path image_dir_;
 }; //class ImageLogger
 
 PLUGINLIB_DECLARE_CLASS(gear_image_handler, ImageLogger,
