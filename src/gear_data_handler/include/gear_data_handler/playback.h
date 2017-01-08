346619b790b4848bd40aebe52abc412c9579fac4
prarobo <kan.prasanna@gmail.com>
2017-01-08 04:06:34 -0500

    Untested version of playback node without camera info

--- src/gear_data_handler/include/gear_data_handler/playback.h
+++ src/gear_data_handler/include/gear_data_handler/playback.h
@@ -7,6 +7,13 @@
 #include <ros/ros.h>
 #include <std_srvs/Trigger.h>
 
+// Boost dependencies
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/recursive_mutex.hpp>
+#include <boost/filesystem.hpp>
+
+namespace fs=boost::filesystem;
+
 namespace gear_data_handler {
 
 class Playback {
@@ -14,7 +21,7 @@ public:
   /**
    * Constructor
    */
-  Playback(ros::NodeHandle nh, ros::NodeHandle pnh);
+  Playback(ros::NodeHandle nh, ros::NodeHandle pnh, int clock_frequency=100);
 
   /**
    * Load all the required parameters
@@ -29,7 +36,7 @@ public:
   /**
    * Parse timestamp from image name
    */
-  double parseTimeStamp(const std::string &image_name);
+  ros::Time parseTimeStamp(const std::string &image_name);
 
   /**
    * Enable playback service callback
@@ -47,20 +54,41 @@ public:
    */
   void play();
 
+  /**
+   * Publish image from disk
+   */
+  void publishImage(const std::multimap <ros::Time, std::string>::iterator &image_it);
+
+  /**
+   * Create a publisher for an image type
+   */
+  void createPublisher(const std::string &dir_name);
+
 private:
   boost::shared_ptr<ros::NodeHandle> nh_, pnh_;
+  ros::ServiceServer enable_playback_;
+  ros::Publisher clock_pub_;
+  std::map<std::string, boost::shared_ptr<image_transport::Publisher>> image_pub_;
+  boost::shared_ptr<image_transport::ImageTransport> it_;
+
   std::string data_dir_;
   std::string subject_id_;
   std::string session_id_;
   std::string activity_id_;
   std::string condition_id_;
   int trial_id_;
+
   double rate_;
+  int clock_frequency_;
+
+  fs::path image_root_dir_;
   std::string image_extn_;
   std::string image_prefix_;
-  std::multimap <double, std::string> image_info_;
+  std::string image_def_;
+  std::multimap <ros::Time, std::string> image_info_;
+
   bool enabled_;
-  ros::ServiceServer enable_playback_;
+  boost::recursive_mutex enable_lock_;
 };
 };
 
--- src/gear_data_handler/src/playback.cpp
+++ src/gear_data_handler/src/playback.cpp
@@ -1,20 +1,36 @@
 #include <gear_data_handler/playback.h>
 
+// ROS dependencies
+#include <rosgraph_msgs/Clock.h>
+#include <cv_bridge/cv_bridge.h>
+
 // Boost dependencies
-#include <boost/filesystem.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
-namespace fs=boost::filesystem;
+// OpenCV dependencies
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 namespace gear_data_handler {
-Playback::Playback(ros::NodeHandle nh, ros::NodeHandle pnh) {
+Playback::Playback(ros::NodeHandle nh, ros::NodeHandle pnh, int clock_frequency) {
+  boost::mutex::scoped_lock(enable_lock_);
+
   // Get node handles
   nh_.reset(new ros::NodeHandle(nh));
   pnh_.reset(new ros::NodeHandle(pnh));
 
   // Enabled
   enabled_ = false;
+
+  // Clock frequency
+  clock_frequency_ = clock_frequency;
+
+  // Time publisher
+  clock_pub_ = nh_->advertise<rosgraph_msgs::Clock>("clock",1);
+
+  // Image transport
+  it_.reset(new image_transport::ImageTransport(*nh_));
 
   // Create service to enable playback
   enable_playback_ = nh_->advertiseService("enable_playback", &Playback::startPlayback, this);
@@ -29,6 +45,43 @@ void Playback::initializePlayback() {
 }
 
 void Playback::play() {
+  boost::mutex::scoped_lock(enable_lock_);
+  if(enabled_) {
+    std::multimap<ros::Time, std::string>::iterator it=image_info_.begin();
+    ros::Time sim_t_current = it->first;
+    ros::Time sim_t_next = it->first+ros::Duration(1/clock_frequency_);
+    ros::Duration t_diff(0);
+
+    while (it != image_info_.end()) {
+      ros::Time sim_next_msg_t = it->first;
+      bool msg_published = false;
+
+      if (sim_next_msg_t<=sim_t_next) {
+        t_diff = ros::Duration((sim_next_msg_t.toSec()-sim_t_current.toSec())/rate_);
+        sim_t_current = sim_next_msg_t;
+
+        // Publish message
+        publishImage(it);
+        it++;
+
+        msg_published = true;
+      }
+
+      if(!msg_published || sim_t_current == sim_t_next) {
+        t_diff = ros::Duration((sim_t_next.toSec()-sim_t_current.toSec())/rate_);
+        sim_t_current = sim_t_next;
+        sim_t_next = sim_t_current+ros::Duration(1/clock_frequency_);
+      }
+
+      // Wait for some time
+      ros::Time::sleepUntil(ros::Time::now()+t_diff);
+
+      // Publish current time
+      rosgraph_msgs::Clock clk;
+      clk.clock = sim_t_current;
+      clock_pub_.publish(clk);
+    }
+  }
 }
 
 bool Playback::startPlayback(std_srvs::Trigger::Request  &req,
@@ -67,24 +120,27 @@ void Playback::loadParams() {
   pnh_->param<std::string>("data_dir", data_dir_, "/mnt/md0/gear_data");
   pnh_->param<std::string>("image_extn", image_extn_, ".jpg");
   pnh_->param<std::string>("image_prefix", image_prefix_, "im");
+  pnh_->param<std::string>("image_def", image_def_, "hd");
 }
 
 void Playback::loadImageInfo() {
 
   // Get all directories where images are present
-  fs::path image_root_dir = fs::path(data_dir_)/fs::path(subject_id_)/fs::path(session_id_)/
-                            fs::path(activity_id_+std::string("_")+condition_id_+std::string("_")+std::to_string(trial_id_))/
-                            fs::path("image");
+  image_root_dir_ = fs::path(data_dir_)/fs::path(subject_id_)/fs::path(session_id_)/
+                    fs::path(activity_id_+std::string("_")+condition_id_+std::string("_")+std::to_string(trial_id_))/
+                    fs::path("image");
 
   // Iterate over all images over directories in image root directory
-  if ( fs::exists(image_root_dir) && fs::is_directory(image_root_dir))  {
-    for( fs::directory_iterator dir_iter(image_root_dir) ; dir_iter != fs::directory_iterator{} ; ++dir_iter) {
+  if ( fs::exists(image_root_dir_) && fs::is_directory(image_root_dir_))  {
+    for( fs::directory_iterator dir_iter(image_root_dir_) ; dir_iter != fs::directory_iterator{} ; ++dir_iter) {
       if (fs::is_directory(dir_iter->status())) {
+        std::string dir_name = dir_iter->path().filename().string();
+        createPublisher(dir_name);
         for( fs::directory_iterator im_iter(*dir_iter) ; im_iter != fs::directory_iterator() ; ++im_iter) {
           if (fs::is_regular_file(im_iter->status()) ) {
             std::string image_name = im_iter->path().filename().string();
-            image_info_.insert(std::multimap<double,std::string>::value_type(parseTimeStamp(image_name),
-                                                                             dir_iter->path().filename().string()));
+            image_info_.insert(std::multimap<ros::Time,std::string>::value_type(parseTimeStamp(image_name),
+                                                                                dir_name));
           }
         }
       }
@@ -92,10 +148,43 @@ void Playback::loadImageInfo() {
   }
 }
 
-double Playback::parseTimeStamp(const std::string &image_name) {
+ros::Time Playback::parseTimeStamp(const std::string &image_name) {
   std::vector<std::string> strs;
   boost::split(strs, image_name, boost::is_any_of("_."));
-  return boost::lexical_cast<double>(strs[1]+std::string(".")+strs[2]);
+  ros::Time t(boost::lexical_cast<uint32_t>(strs[1]), boost::lexical_cast<uint32_t>(strs[2]));
+  return t;
+}
+
+void Playback::publishImage(const std::multimap <ros::Time, std::string>::iterator &image_it) {
+  // Get image file path
+  std::string image_name = image_prefix_+std::to_string(image_it->first.sec)
+                           +std::string("_")+std::to_string(image_it->first.nsec)
+                           +image_extn_;
+  fs::path image_path = image_root_dir_/fs::path(image_it->second)/fs::path(image_name);
+
+  // Load image
+  cv_bridge::CvImage cv_image;
+  cv_image.image = cv::imread(image_path.string(),CV_LOAD_IMAGE_COLOR);
+  cv_image.encoding = "bgr8";
+  sensor_msgs::Image ros_image;
+  cv_image.toImageMsg(ros_image);
+
+  // Publish
+  image_pub_.at(image_it->second)->publish(ros_image);
+}
+
+void Playback::createPublisher(const std::string &dir_name) {
+  // Check if the specified publisher exists
+  if(image_pub_.find(dir_name) == image_pub_.end()) {
+
+    // Parse directory name to get publisher details
+    std::vector<std::string> strs;
+    boost::split(strs, dir_name, boost::is_any_of("_"));
+    std::string msg_name = std::string("/")+strs[0]+std::string("/")+image_def_
+                           +std::string("/")+std::string("image_")+strs[1];
+    boost::shared_ptr<image_transport::Publisher> pub(new image_transport::Publisher(it_->advertise(msg_name, 1)));
+    image_pub_[dir_name] = pub;
+  }
 }
 
 };
@@ -105,5 +194,8 @@ int main(int argc, char **argv){
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
   gear_data_handler::Playback player(nh, pnh);
-  ros::spin();
+  while(ros::ok()) {
+    player.play();
+    ros::spinOnce();
+  }
 }
