924d9309d63e39870b8a08faf5e9435313f0b1a5
prarobo <kan.prasanna@gmail.com>
2017-01-27 16:28:52 -0500

    Added image view to launcher script

--- bin/gear_playback.sh
+++ bin/gear_playback.sh
@@ -6,5 +6,8 @@ sleep 1
 roslaunch gear_launch gear.launch do_pointcloud:=false enable_loggers:=false enable_trackers:=false use_sim_time:=true &
 sleep 1
 
+rosrun rqt_image_view rqt_image_view &
+sleep 1
+
 rosrun gear_playback gear_playback
 fg
