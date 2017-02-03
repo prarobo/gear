136927d658d307aab00a098df5cc24528e905507
prarobo <kan.prasanna@gmail.com>
2017-02-02 20:57:28 -0500

    Working diagnostics

--- src/gear_diagnostics/include/gear_diagnostics/message_monitor.h
+++ src/gear_diagnostics/include/gear_diagnostics/message_monitor.h
@@ -17,6 +17,7 @@
 #include <diagnostic_updater/diagnostic_updater.h>
 #include <diagnostic_updater/publisher.h>
 #include <std_msgs/Int64.h>
+#include <sensor_msgs/CameraInfo.h>
 
 // Boost Dependencies
 #include <boost/shared_ptr.hpp>
@@ -33,9 +34,14 @@ public:
   /*
    * Destructor
    */
-  ~MessageMonitor(){};
+  virtual ~MessageMonitor(){};
 
-private:
+  /*
+   * Initalization function
+   */
+  void initialize(const std::string &hw_id);
+
+protected:
   /*
    * Load parameters from parameter server
    */
@@ -44,33 +50,26 @@ private:
   /*
    * Setup diagnostics components
    */
-  void setupDiagnostics();
+  void setupDiagnostics(const std::string &hw_id);
 
   /*
    * Setup topic subscribers
    */
-  void setupSubscribers();
+  virtual void setupSubscribers()=0;
 
   /*
    * Update diagnostics function
    */
   void updateDiagnostics(const ros::TimerEvent& t);
 
-  /*
-   * Register the arrival of a image count message
-   */
-  void registerEvent(const ros::MessageEvent<std_msgs::Int64 const>& event);
-
   // Default parameters
-  std::string default_synchronizer_topic_;
-  std::vector<std::string> default_sensor_logger_topics_;
+  std::vector<std::string> default_data_topics_;
   int default_frame_rate_, default_window_size_;
   double default_frequency_tolerance_, default_min_acceptable_delay_, default_max_acceptable_delay_;
   double default_update_interval_;
 
   // Parameters
-  std::string synchronizer_topic_;
-  std::vector<std::string> sensor_logger_topics_;
+  std::vector<std::string> data_topics_;
   int frame_rate_, window_size_;
   double min_frequency_, max_frequency_;
   double frequency_tolerance_, min_acceptable_delay_, max_acceptable_delay_;
@@ -80,18 +79,65 @@ private:
   boost::shared_ptr<ros::NodeHandle> pnh_;
 
   // Diagnostics updaters
-  diagnostic_updater::Updater synchronizer_updater_, logger_updater_;
-  boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> synchronizer_diagnostics_;
-  std::vector<boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic>> sensor_logger_diagnostics_;
+  diagnostic_updater::Updater data_updater_;
+  std::vector<boost::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic>> data_diagnostics_;
 
   // Subscribers
-  boost::shared_ptr<ros::Subscriber> synchronizer_sub_;
-  std::vector<boost::shared_ptr<ros::Subscriber>> sensor_logger_sub_;
+  std::vector<boost::shared_ptr<ros::Subscriber>> data_sub_;
 
   //Timer
   ros::Timer diagnostics_timer_;
 };
 
+class LoggerMonitor: public MessageMonitor {
+public:
+  /*
+   * Constructor
+   */
+  LoggerMonitor();
+
+  /*
+   * Destructor
+   */
+  ~LoggerMonitor(){};
+
+private:
+  /*
+   * Setup topic subscribers
+   */
+  void setupSubscribers();
+
+  /*
+   * Register the arrival of a image count message
+   */
+  void registerEvent(const ros::MessageEvent<std_msgs::Int64 const>& event);
+
+};
+
+class CameraMonitor: public MessageMonitor {
+public:
+  /*
+   * Constructor
+   */
+  CameraMonitor();
+
+  /*
+   * Destructor
+   */
+  ~CameraMonitor(){};
+
+private:
+  /*
+   * Register the arrival of a image count message
+   */
+  void registerEvent(const ros::MessageEvent<sensor_msgs::CameraInfo const>& event);
+
+  /*
+   * Setup topic subscribers
+   */
+  void setupSubscribers();
+
+};
 } // Namespace gear_dignostics
 
 #endif /* MESSAGE_MONITOR_H_ */
