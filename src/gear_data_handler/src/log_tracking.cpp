72911f0a211e1131e4045b954c95ca69f64f4df4
prarobo <kan.prasanna@gmail.com>
2017-02-20 22:04:55 -0500

    Added logging tag information as text (untested)

--- src/gear_data_handler/src/log_tracking.cpp
+++ src/gear_data_handler/src/log_tracking.cpp
@@ -32,6 +32,11 @@ void TrackingLogger::aprilTrackingCallback(const apriltags_ros::AprilTagDetectio
     // Saving to disk
     bag_.write("tracking_info", ros::Time::now(), msg);
 
+    for(const auto &m: msg.detections) {
+      // Log to text file
+      logText(m.pose);
+    }
+
     // Publish running image counts
     publishCount();
   }
@@ -46,9 +51,43 @@ void TrackingLogger::arTrackingCallback(const ar_track_alvar_msgs::AlvarMarkers&
     // Saving to disk
     bag_.write("tracking_info", ros::Time::now(), msg);
 
+    for(const auto &m: msg.markers) {
+      // Log to text file
+      logText(m.pose);
+    }
+
     // Publish running image counts
     publishCount();
   }
+}
+
+void TrackingLogger::logText(const geometry_msgs::PoseStamped& msg) {
+  // Get the tf transformation
+  geometry_msgs::PoseStamped pose_out;
+  tf_listener_.transformPose("world", msg, pose_out);
+
+  // Write to text file
+  text_fp_<<msg.header.stamp.toSec()
+          <<"\t"<<msg.pose.position.x<<"\t"<<msg.pose.position.y<<"\t"<<msg.pose.position.z
+          <<"\t"<<msg.pose.orientation.x<<"\t"<<msg.pose.orientation.y<<"\t"<<msg.pose.orientation.z
+          <<"\t"<<msg.pose.orientation.w<<std::endl;
+}
+
+bool TrackingLogger::toggleLogger(std_srvs::SetBool::Request  &req,
+                                  std_srvs::SetBool::Response &res){
+
+  bool out = false;
+
+  // Open rosbag
+  if (req.data) {
+    out = BagLogger::toggleLogger(req, res);
+    boost::filesystem::path text_path = image_dir_/boost::filesystem::path(image_type_+".txt");
+    text_fp_.open(text_path.string());
+  } else {
+    text_fp_.close();
+  }
+
+  return out;
 }
 
 };
